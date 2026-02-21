ec9a453d40cdfd1ffce538016914f60ad33e9d3f
Armand <armand.vieau@epita.fr>
2026-02-21 14:40:33 +0100

    Environment Variables handling added

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,4 @@
 const env = process.env.NODE_ENV || "dev";
-
-require("dotenv").config({ path: `.env.${env}` });
-
+require("dotenv").config({ path: `.env.${env}`});
 const config = require(`./${env}`);
-
 module.exports = config;
